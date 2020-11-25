359d48a3f77b55223d6883836c9ee59326270383
Fabio Andreis <andreisfc@protonmail.com>
2020-11-25 03:16:14 -0300

    Initial prototype, single post

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
-# Create your views here.
+
+def home(request):
+    return render(
+        request,
+        'mainpage/home.html',
+        {
+        'title': 'Post #1 sobre C. neoformans',
+        'author': 'FCA',
+        'organism': 'Cryptococcus neoformans',
+        'created': datetime.now(),
+        'lastmod': datetime.now(),
+        }
+    )
