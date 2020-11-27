b17855c1d3a621817a15f3e3cd7da701bfaeb4fc
Fabio Andreis <andreisfc@protonmail.com>
2020-11-27 19:32:38 -0300

    Prototype: multiple posts

--- mainpage/models.py
+++ mainpage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    creationdate = models.DateTimeField()
+    updatedate = models.DateTimeField()
+    organism = models.CharField(max_length=100)
+    text = models.TextField()
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+
+from mainpage.models import Post
+
+posts = [Post(
+    title="Post 1", author="FCA", creationdate=datetime.now(tz=timezone(-timedelta(hours=3))), updatedate=datetime.now(tz=timezone(-timedelta(hours=3))), organism="Cryptococcus neoformans", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus in massa tempor nec feugiat nisl pretium fusce. Enim sed faucibus turpis in eu mi bibendum neque egestas. Id aliquet risus feugiat in ante metus dictum at tempor. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Massa enim nec dui nunc mattis enim ut. Dolor sit amet consectetur adipiscing elit. Iaculis at erat pellentesque adipiscing commodo elit. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Praesent tristique magna sit amet purus gravida quis blandit turpis. Tincidunt tortor aliquam nulla facilisi cras. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Mauris sit amet massa vitae tortor condimentum lacinia quis"
+),
+Post(
+    title="Post 2", author="FCA", creationdate=datetime.now(tz=timezone(-timedelta(hours=3))), updatedate=datetime.now(tz=timezone(-timedelta(hours=3))), organism="Metarhizium anisopliae", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus in massa tempor nec feugiat nisl pretium fusce. Enim sed faucibus turpis in eu mi bibendum neque egestas. Id aliquet risus feugiat in ante metus dictum at tempor. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Massa enim nec dui nunc mattis enim ut. Dolor sit amet consectetur adipiscing elit. Iaculis at erat pellentesque adipiscing commodo elit. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Praesent tristique magna sit amet purus gravida quis blandit turpis. Tincidunt tortor aliquam nulla facilisi cras. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Mauris sit amet massa vitae tortor condimentum lacinia quis"
+),
+Post(
+    title="Post 3", author="FCA", creationdate=datetime.now(tz=timezone(-timedelta(hours=3))), updatedate=datetime.now(tz=timezone(-timedelta(hours=3))), organism="Cryptococcus neoformans", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus in massa tempor nec feugiat nisl pretium fusce. Enim sed faucibus turpis in eu mi bibendum neque egestas. Id aliquet risus feugiat in ante metus dictum at tempor. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Massa enim nec dui nunc mattis enim ut. Dolor sit amet consectetur adipiscing elit. Iaculis at erat pellentesque adipiscing commodo elit. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Praesent tristique magna sit amet purus gravida quis blandit turpis. Tincidunt tortor aliquam nulla facilisi cras. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Mauris sit amet massa vitae tortor condimentum lacinia quis"
+),
+Post(
+    title="Post 4", author="FCA", creationdate=datetime.now(tz=timezone(-timedelta(hours=3))), updatedate=datetime.now(tz=timezone(-timedelta(hours=3))), organism="Metarhizium anisopliae", text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus in massa tempor nec feugiat nisl pretium fusce. Enim sed faucibus turpis in eu mi bibendum neque egestas. Id aliquet risus feugiat in ante metus dictum at tempor. Laoreet id donec ultrices tincidunt arcu non sodales neque sodales. Massa enim nec dui nunc mattis enim ut. Dolor sit amet consectetur adipiscing elit. Iaculis at erat pellentesque adipiscing commodo elit. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Praesent tristique magna sit amet purus gravida quis blandit turpis. Tincidunt tortor aliquam nulla facilisi cras. Ut tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Mauris sit amet massa vitae tortor condimentum lacinia quis"
+)]
 
 
 def home(request):
     return render(
         request,
         'mainpage/home.html',
-        {
-        'title': 'Post #1 sobre C. neoformans',
-        'author': 'FCA',
-        'organism': 'Cryptococcus neoformans',
-        'created': datetime.now(),
-        'lastmod': datetime.now(),
-        }
+        {'posts':posts}
     )
